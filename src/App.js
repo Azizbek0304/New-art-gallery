7fc0f8b6fb264f233ab9fc3ebebc6cfc07eaef77
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-30 22:38:18 +0500

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Testimonial from './pages/Testimonal';
+import Contact from './pages/Contact';
 import './style.css';
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/service" exact component={Services} />
+        <Route path="/testimonial" exact component={Testimonial} />
+        <Route path="/contact" exact component={Contact} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
   rootElement
 );
