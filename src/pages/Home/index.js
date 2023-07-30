668c140414c27a8c4573896cf7a4c3d1af5ee00d
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-30 22:46:59 +0500

    1 file modified

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar'
+import React from 'react';
 
 const Home = () => {
   return (
     <div className="container">
-    
-    <h1 className="text-center" style={{paddingTop: "30%"}}>
-      Home
-    </h1>
-    <Navbar />
-  </div>
-  )
-}
+      <h1 className="text-center" style={{ paddingTop: '30%' }}>
+        Home
+      </h1>
+    </div>
+  );
+};
 export default Home;
