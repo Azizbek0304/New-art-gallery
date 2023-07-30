8730f3aadb69cda7553789b05422479ad3ea173d
Azizbek Khursanov <115786719+Azizbek0304@users.noreply.github.com>
2023-07-30 22:40:26 +0500

    Add files via upload

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+
+const Home = () => {
+  return (
+    <div className="container">
+    
+    <h1 className="text-center" style={{paddingTop: "30%"}}>
+      Home
+    </h1>
+    <Navbar />
+  </div>
+  )
+}
+export default Home;
