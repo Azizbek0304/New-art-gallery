2291128e65e0f22ae60862a48f1c9f8b709f39b9
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-30 22:48:58 +0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
+
 ReactDOM.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-  rootElement
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
