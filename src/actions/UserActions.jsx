9d1e9c762260afa9ce51c2c1b0465b48103294d6
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-31 10:56:59 +0500

    3 files modified

--- src/actions/UserActions.jsx
+++ src/actions/UserActions.jsx
@@ -7,6 +7,10 @@ export const USER_SIGNUP_FAIL = 'USER_SIGNUP_FAIL';
 export const USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST';
 export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
 export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL';
+export const USER_LOGOUT = 'USER_LOGOUT';
+export const UPLOAD_FILE_REQUEST = 'UPLOAD_FILE_REQUEST';
+export const UPLOAD_FILE_SUCCESS = 'UPLOAD_FILE_SUCCESS';
+export const UPLOAD_FILE_FAIL = 'UPLOAD_FILE_FAIL';
 
 // Signup Action
 export const signup = (formData) => async (dispatch) => {
@@ -19,6 +23,7 @@ export const signup = (formData) => async (dispatch) => {
       },
     };
 
+    // Replace '/api/signup' with your actual signup endpoint
     const { data } = await axios.post('/api/signup', formData, config);
 
     dispatch({ type: USER_SIGNUP_SUCCESS, payload: data });
@@ -44,12 +49,45 @@ export const login = (formData) => async (dispatch) => {
       },
     };
 
+    // Replace '/api/login' with your actual login endpoint
     const { data } = await axios.post('/api/login', formData, config);
 
     dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// Logout Action
+export const logout = () => (dispatch) => {
+  // Add code to handle logout on the server if needed
+  dispatch({ type: USER_LOGOUT });
+};
+
+// Upload Action
+export const uploadFile = (formData) => async (dispatch) => {
+  try {
+    dispatch({ type: UPLOAD_FILE_REQUEST });
+
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+
+    // Replace '/api/upload' with your actual upload endpoint
+    const { data } = await axios.post('/api/upload', formData, config);
+
+    dispatch({ type: UPLOAD_FILE_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: UPLOAD_FILE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -5,13 +5,19 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
-} from '../actions/userActions';
+  USER_LOGOUT,
+  UPLOAD_FILE_REQUEST,
+  UPLOAD_FILE_SUCCESS,
+  UPLOAD_FILE_FAIL,
+} from '../../actions/userActions';
 
 // Initial state
 const initialState = {
   user: null,
   error: null,
   loading: false,
+  isAuthor: false,
+  uploadedFile: null,
 };
 
 // User Reducer
@@ -19,9 +25,11 @@ export const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case USER_SIGNUP_REQUEST:
     case USER_LOGIN_REQUEST:
+    case UPLOAD_FILE_REQUEST:
       return {
         ...state,
         loading: true,
+        error: null,
       };
     case USER_SIGNUP_SUCCESS:
     case USER_LOGIN_SUCCESS:
@@ -33,10 +41,23 @@ export const userReducer = (state = initialState, action) => {
       };
     case USER_SIGNUP_FAIL:
     case USER_LOGIN_FAIL:
+    case UPLOAD_FILE_FAIL:
       return {
         ...state,
         loading: false,
         error: action.payload,
+      };
+    case USER_LOGOUT:
+      return {
+        ...state,
+        user: null,
+        isAuthor: false,
+      };
+    case UPLOAD_FILE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        uploadedFile: action.payload,
       };
     default:
       return state;
