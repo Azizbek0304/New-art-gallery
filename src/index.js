412d8d43db7ba9f11751c7c2d70cc67c6636aa66
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-02 16:34:01 +0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'; // Import Provider from react-redux
-import store from './store'; // Import your Redux store
+// import { Provider } from 'react-redux'; // Import Provider from react-redux
+// import store from './store'; // Import your Redux store
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  // </Provider>
 );
