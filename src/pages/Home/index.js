2aeb3cd25710713a8d50070ca04af971cf4ac553
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-02 22:17:19 +0500

    1 file modified

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import CreativeSwiperComponent from '../../components/CreativeSwiperComponent'
+// import CreativeSwiperComponent from '../../components/CreativeSwiperComponent';
 
 const Home = () => {
   return (
     <div className="container">
-      <CreativeSwiperComponent />
+      <h1>home</h1>
+      {/* <CreativeSwiperComponent /> */}
     </div>
   );
 };
